42aa4a4afa9a34ba36be46aeeb0ddb759625a111
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-15 16:57:08 +0300

    End points

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ import pandas as pd
 import ast
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+    #methods go here
+    pass
+
+
+class Locations(Resources):
+    #methods go here
+    pass
+
+api.add_resource(Users, '/users') # '/users' is our entry point for Users
+api.add_resource(Locations, '/locations') # and '/locations' iis our entry point
